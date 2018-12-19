499ca91a7af0ee7695d1d82eb834ee2df0e715e6
mxochicale <perez.xochicale@gmail.com>
2018-12-19 21:28:59 +0000

    [code] tidy-up

---
+++ code/rscripts/C_RQAEntr.R
@@ -0,0 +1,80 @@
+###############################################################################	
+#
+# RQAEntr for openface landmarks 
+# 
+#
+#
+#
+#
+# Miguel P Xochicale [http://mxochicale.github.io]
+#
+###############################################################################	
+	# OUTLINE:
+	# (0) Loading libraries and functions
+	# (1) Defining paths for main_path, r_scripts_path, ..., etc.
+	# (2) Setting DataSets paths and reading data
+	# (3) Reading data
+	
+
+
+#################
+# Start the clock!
+start.time <- Sys.time()
+
+
+################################################################################
+# (0) Loading Functions and Libraries and Setting up digits
+library(data.table) # for manipulating data
+library(ggplot2) # for plotting 
+
+
+
+################################################################################
+# (1) Defining paths for main_path, r_scripts_path, ..., etc.
+
+homepath <- Sys.getenv("HOME") 
+r_scripts_path <- getwd()
+setwd("../../")
+repository_path <- getwd()
+
+
+##VERSION 
+version <- '00'
+feature_path <- '/dataset'
+graphics_path <- '/rqaentr'
+
+#### Outcomes Data Path
+outcomes_graphics_path <- paste(repository_path,'/docs/figs', graphics_path, sep="")
+
+### DataSet Path
+data_path <- paste(repository_path,'/data', feature_path, '/v', version, sep="")
+
+
+
+################################################################################
+# (2) Setting DataSets paths and reading data
+setwd(data_path)
+
+
+################################################################################
+# (3) Reading data
+file_ext <- "rawopenfacedata.datatable"
+xdata <- fread( file_ext, header=TRUE)
+
+
+
+######################################################
+# If you come across to this part on how to compute RQAEntr, 
+# you can email me (perezDOTxochicaleATgmailDOTcom) subjet:RQAEntr
+# or go to code at https://github.com/mxochicale/phd-thesis-code-data
+
+
+
+#################
+# Stop the clock!
+end.time <- Sys.time()
+end.time - start.time
+
+################################################################################
+setwd(r_scripts_path) ## go back to the r-script source path
+
